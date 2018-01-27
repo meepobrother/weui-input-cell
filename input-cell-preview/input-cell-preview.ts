d7540e35fedd3e7c86e7aaf642787735a2ed056a
15138730370 <1037483576@qq.com>
2018-01-27 21:43:14 +0800

    update to save

--- input-cell-preview/input-cell-preview.ts
+++ input-cell-preview/input-cell-preview.ts
@@ -4,6 +4,11 @@ import {
     HostListener
 } from '@angular/core';
 import { ReactComponent } from 'ng-react-component';
+import { fromEvent } from 'rxjs/observable/fromEvent';
+import { merge } from 'rxjs/observable/merge';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/debounceTime';
+
 
 export interface InputCellProps {
     title?: string;
@@ -13,6 +18,7 @@ export interface InputCellProps {
     children: any[];
     style?: { [k: string]: string };
     focus?: boolean;
+    value?: string;
 }
 
 @Component({
@@ -20,7 +26,7 @@ export interface InputCellProps {
     templateUrl: './input-cell-preview.html',
     styleUrls: ['./input-cell-preview.scss']
 })
-export class InputCellPreviewComponent extends ReactComponent<InputCellProps, any> {
+export class InputCellPreviewComponent extends ReactComponent<InputCellProps, any> implements OnInit {
     @ViewChild('input') input: ElementRef;
     @HostListener('mouseenter', ['$event'])
     mouseover() {
@@ -30,6 +36,7 @@ export class InputCellPreviewComponent extends ReactComponent<InputCellProps, an
     mouseleave() {
         this.props.focus = false;
     }
+
     constructor(
         _differs: KeyValueDiffers,
         _ele: ElementRef,
@@ -38,7 +45,26 @@ export class InputCellPreviewComponent extends ReactComponent<InputCellProps, an
         super(_differs, _ele, _render);
     }
 
-    onPropsChange() { }
+    ngOnInit() {
+        merge(
+            fromEvent(this.ele.nativeElement, 'change'),
+            fromEvent(this.ele.nativeElement, 'propertychange'),
+            fromEvent(this.ele.nativeElement, 'keyup'),
+            fromEvent(this.ele.nativeElement, 'keydown'),
+        ).distinctUntilChanged()
+            .debounceTime(300)
+            .subscribe(res => {
+                this.props.value = this.input.nativeElement.value;
+                this.setProps({
+                    ...this.props,
+                    ...{ value: this.input.nativeElement.value }
+                });
+            });
+    }
+
+    onPropsChange() {
+        console.log(this.props);
+    }
 
     onStateChange() { }
 }
